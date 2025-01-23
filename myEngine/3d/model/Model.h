d79fd53a29d3c5faef794961f5b07c22794a8d5c
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2025-01-23 11:40:35 +0900

    モデルのテクスチャ変更

--- myEngine/3d/model/Model.h
+++ myEngine/3d/model/Model.h
@@ -68,6 +68,7 @@ public:
 	void SetAnimator(Animator* animator) { animator_ = animator; }
 	void SetSkin(Skin* skin) { skin_ = skin; }
 	void SetBone(Bone* bone) { bone_ = bone; }
+	void SetTextureIndex(const std::string& filePath);
 
 	ModelData GetModelData() { return modelData; }
 

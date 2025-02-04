a029f46db2b1199c4531060cc69ef1e88d5b187c
hagiwara hibiki <146906515+hibiki128@users.noreply.github.com>
2025-02-04 16:13:27 +0900

    プレイヤーの向き修正

--- application/Object/Player/Player.cpp
+++ application/Object/Player/Player.cpp
@@ -121,6 +121,14 @@ void Player::Update(MapChipField *mapChipField) {
     } else {
         BaseObject::SetAnima("animation/playerJump.gltf");
     }
+
+    if (velocity_.x > 0) {
+        BaseObject::SetRotationY(degreesToRadians(90.0f));
+    }
+    if (velocity_.x < 0) {
+        BaseObject::SetRotationY(degreesToRadians(-90.0f));
+    }
+
     BaseObject::Update();
     ///
     ///	重力を常に受ける
@@ -468,12 +476,6 @@ void Player::HandleInput() {
         }
         if (input_->PushKey(DIK_D)) {
             velocity_.x = kMoveSpeed;
-        }
-        if (input_->TriggerKey(DIK_D)) {
-            BaseObject::SetRotationY(degreesToRadians(90.0f));
-        }
-        if (input_->TriggerKey(DIK_A)) {
-            BaseObject::SetRotationY(degreesToRadians(-90.0f));
         }
     }
 
